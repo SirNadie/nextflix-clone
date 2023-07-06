63fe606fd6a0a373462d1def667cb5d351188134
Marc Rodríguez <136878993+SirNadie@users.noreply.github.com>
2023-07-06 10:04:00 -0400

    github auth ready

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -101,10 +101,11 @@ const Auth = () => {
                   hover:opacity-80
                   transition
                   "
-               >
-                <FcGoogle size={30}/>
+              >
+                <FcGoogle size={30} />
               </div>
               <div
+                onClick={() => signIn('github', { callbackUrl: '/' })}
                 className="
                   w-10
                   h-10
@@ -117,8 +118,8 @@ const Auth = () => {
                   hover:opacity-80
                   transition
                   "
-               >
-                <FaGithub size={30}/>
+              >
+                <FaGithub size={30} />
               </div>
             </div>
             <p className="text-neutral-500 mt-12">
